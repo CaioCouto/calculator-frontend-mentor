ca52e187a1c8b721ea0339edc64c88c14155d0c2
Caio Couto <coutocaio3@gmail.com>
2023-02-25 21:15:43 -0300

    Fixed bug - added forgotten '+1'

--- static/js/classes/ThemeSelector.js
+++ static/js/classes/ThemeSelector.js
@@ -39,8 +39,8 @@ export default class ThemeSelector {
         this.__removeClass(this.__switchBtns[lastIndex].querySelector('div'), 'active');
         this.__addClass(this.__switchBtns[this.__activeDivIndex].querySelector('div'), 'active');
         this.__elements.forEach(element => {
-            this.__removeClass(element, `theme${lastIndex}`);
-            this.__addClass(element, `theme${this.__activeDivIndex}`);
+            this.__removeClass(element, `theme${lastIndex+1}`);
+            this.__addClass(element, `theme${this.__activeDivIndex+1}`);
         });
         this.__saveTheme();
     }
